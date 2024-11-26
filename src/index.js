4644b85bddeb03e6b05861e3ccc7a18dd62d7f88
Michelitossilva <michelssilva@gmail.com>
2024-11-26 15:47:53 -0300

    Atualiza build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Nova API do React 18 para renderização
+import './index.css'; // Importa os estilos globais
+import App from './App'; // Componente principal da aplicação
+import reportWebVitals from './reportWebVitals'; // Ferramenta para medir desempenho
 
+// Seleciona o elemento raiz no HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza o componente principal dentro do <div id="root">
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Opção para medir desempenho e enviar dados para um endpoint de analytics
+reportWebVitals(console.log);
